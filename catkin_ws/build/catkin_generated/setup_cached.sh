471b8c92a4eaebacd1ce6f8c93eae7754f80acf6
hs <doug@humanitas.io>
2020-06-10 20:52:31 -0400

    finished preliminary hardcoded overlord node

--- catkin_ws/build/catkin_generated/setup_cached.sh
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -7,6 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export LD_LIBRARY_PATH="/opt/ros/melodic/lib"
-export PKG_CONFIG_PATH="/opt/ros/melodic/lib/pkgconfig"
 export PWD="/home/hs/Documents/pendulum_control/catkin_ws/build"
---
+++ catkin_ws/build/pendulum_pkg/CMakeFiles/overlord_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/overlord_node.dir/src/overlord_node.cpp.o"
+  "/home/hs/Documents/pendulum_control/catkin_ws/devel/lib/pendulum_pkg/overlord_node.pdb"
+  "/home/hs/Documents/pendulum_control/catkin_ws/devel/lib/pendulum_pkg/overlord_node"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/overlord_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
