39597aef2489d88e85674503b444121e17617614
hs <doug@humanitas.io>
2020-06-12 00:03:17 -0400

    visualizing the 5 penulums in rviz

--- catkin_ws/build/catkin_generated/setup_cached.sh
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -7,4 +7,7 @@
 # new environment variables
 
 # modified environment variables
+export CMAKE_PREFIX_PATH="/home/hs/Documents/pendulum_control/catkin_ws/devel:$CMAKE_PREFIX_PATH"
 export PWD="/home/hs/Documents/pendulum_control/catkin_ws/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/hs/Documents/pendulum_control/catkin_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/hs/Documents/pendulum_control/catkin_ws/src:$ROS_PACKAGE_PATH"
---
+++ catkin_ws/build/pendulum_pkg/CMakeFiles/ui_bridge_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ui_bridge_node.dir/src/ui_bridge_node.cpp.o"
+  "/home/hs/Documents/pendulum_control/catkin_ws/devel/lib/pendulum_pkg/ui_bridge_node.pdb"
+  "/home/hs/Documents/pendulum_control/catkin_ws/devel/lib/pendulum_pkg/ui_bridge_node"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ui_bridge_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
