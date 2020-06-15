8a9c6b085aa653d14547b9327465e478ec435f35
hs <doug@humanitas.io>
2020-06-15 10:16:55 -0400

    fixed small errors in README

--- catkin_ws/src/pendulum_pkg/src/controller_node.cpp
+++ catkin_ws/src/pendulum_pkg/src/controller_node.cpp
@@ -1,26 +1,17 @@
 #include "controller.h"
 #include <cstdlib>
 
-
-/*****************************************************************************/
-/*****************************************************************************/
 int main(int argc, char* argv[])
 {
-    /* Init "ROS" */
 	ros::init(argc, argv, "controller_node");
 	ros::NodeHandle NodeHandle;
     
-
-
     float ctrl_rate;
     float ctrl_rate_default = 40.0;
     NodeHandle.param("ctrl_rate", ctrl_rate, ctrl_rate_default);
 
     controller ctrl = controller(NodeHandle,ctrl_rate);
-
-
-        //ROS_INFO("running!");
-        ctrl.run();
+    ctrl.run();
 
     return EXIT_SUCCESS;
 }
